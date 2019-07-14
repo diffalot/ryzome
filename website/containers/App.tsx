e35abd6d1cb265dbefa3e3732faeb5fd77e79580
Alice Davis <alice@medium.com>
2019-07-14 14:20:21 -0700

    init

--- website/containers/App.tsx
+++ website/containers/App.tsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, ReactElement } from 'react'
+import React, { useState, useEffect, ReactElement } from "react";
 
-import { root } from '@ryzome/root'
+import { root } from "@ryzome/root";
 
-import { Toolbar } from './Toolbar'
+import { Toolbar } from "./Toolbar";
 
 export const App = (): ReactElement => {
-  const [message, setMessage] = useState('welcome')
+  const [message, setMessage] = useState("welcome");
 
   useEffect((): void => {
     root().then((rootMessage): void => {
-      setMessage(rootMessage)
-    })
-  })
+      setMessage(rootMessage);
+    });
+  });
 
   return (
     <div>
       <Toolbar />
       <p>{message}</p>
     </div>
-  )
-}
+  );
+};
